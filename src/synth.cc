d969542aa55206880dc127682156f8b0d31eafd8
Ultros <redhate@ymail.com>
2021-02-17 05:50:46 -0500

    Update synth.cc
    Removed foul mouth language from commenting heh

--- src/synth.cc
+++ src/synth.cc
@@ -1,3 +1,4 @@
+
 //*************************************************************************
 // File name: synth.cc
 // Wizards's Polyphonic Synthesis Sample.
@@ -8,61 +9,68 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "../include/Nco.h"
+#include "src/Nco.h"
+
+#define MAX (32768)
+#define RATE (24000)
 
 int main(int argc, char **argv)
 {
 	
-	if(argc < 2){
-		//if you see this you're a fuckin idiot do it right idiot.
+	//first of all let's make sure we even have the correct number of arguments.
+	if(argc < 2){ 
 		printf("Usage: $s freq1, freq2, freq3 | aplay -f s16_le -r 11520\r\n");
 		exit(0);
 	}
 	
-	//no sense in even getting here until have done the argc sanity checks, why begin to populate memory before then?
+	//now we declare our vars, after making sure the program even has the correct args
+	//no sense in populating the ram with anything before checking to see if the params
+	//are even correct...
 	int16_t sample;
 	float floatSample;
 	float iValue, qValue;
-	Nco *ncoPtr1, *ncoPtr2, *ncoPtr3;
+	Nco *ncoPtr[3];
 
-	//more checks and assignment
-	if(argc >= 2) ncoPtr1 = new Nco(24000,atof(argv[1]));
-	if(argc >= 3) ncoPtr2 = new Nco(24000,atof(argv[2]));
-	if(argc >= 4) ncoPtr3 = new Nco(24000,atof(argv[3]));
+	//if tail param (s), assign pointer and set frequency value 
+	if(argc >= 2) ncoPtr[0] = new Nco(RATE,atof(argv[1]));
+	if(argc >= 3) ncoPtr[1] = new Nco(RATE,atof(argv[2]));
+	if(argc >= 4) ncoPtr[2] = new Nco(RATE,atof(argv[3]));
 	
 	//i like to localise vars to avoid confusion and mixup and my own sanity...
 	int i;
-	for (i = 0; i < 32768; i++)
-	{
+	for (i=0;i<MAX;i++){
 
-		//toneA
-		if(argc >= 2) ncoPtr1->run(&iValue,&qValue);
+		//tone A mutation
+		if(argc >= 2) ncoPtr[0]->run(&iValue,&qValue);
 		if(argc >= 2) floatSample = iValue;
-		//toneB
-		if(argc >= 3) ncoPtr2->run(&iValue,&qValue);
+		
+		//tone B mutation
+		if(argc >= 3) ncoPtr[1]->run(&iValue,&qValue);
 		if(argc >= 3) floatSample += iValue;
-		//toneC
-		if(argc >= 4) ncoPtr3->run(&iValue,&qValue);
+		
+		//tone C mutation
+		if(argc >= 4) ncoPtr[2]->run(&iValue,&qValue);
 		if(argc >= 4) floatSample += iValue;
 		
 		//count
-		floatSample *= 32767;
+		floatSample *= MAX-1;
 		floatSample /= argc;
 		
-		//cast it.
+		//cast it to the correct type..
 		sample = (int16_t)floatSample;
-		//write it to stdout to be picked up by: | aplay -f s16_le -r 11520
+		
+		//write it to stdout to be picked up by: | aplay -f s16_le -r 24000
 		fwrite(&sample,sizeof(int16_t),1,stdout);
 
 	}
 	
 	//clean up
-	if(argc >= 2) delete ncoPtr1;
-	if(argc >= 3) delete ncoPtr2;
-	if(argc >= 4) delete ncoPtr3;
-
+	//int i already declared once above..
+	for(i=0;i<argc-1;i++) delete ncoPtr[i];
+	
 	//return 0 like a human being...
 	return 0;
 
 } 
+
 
